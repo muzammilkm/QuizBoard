f880e5ee12d06eb31ce89ac57bdf3d2ae2acc0ca
muzammilkm <muzammilkm@gmail.com>
2016-06-30 18:33:14 +0530

    Initial

---
+++ QuizBoard/app/login/login.controller.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="D:\Rangaveni\AngularjsDemo\QuizBoard\QuizBoard\Scripts/angular.min.js" />
+angular(function () {
+    angular.module("quizBoardApp", ['ui-router'])
+    .controller("quizBoardAppController", ['$rootScope']);
+})
+(angular);
