4ed084a011835dbeb521b67f144d8177cddfcfe4
sripriyanka8392 <priyachinnu14@gmail.com>
2016-07-19 12:43:49 +0530

    Displaying questions in results page

--- QuizBoard/app/results/results.controller.js
+++ QuizBoard/app/results/results.controller.js
@@ -7,8 +7,22 @@
         var vm = this;
 
         //Properties
+        vm.responseIndex = 0;
         vm.user = $stateParams.user;
+        var indexres = 1;
+        var responseindex =0;
         vm.responseList = $stateParams.responseList;
+        if (vm.responseList.length != null) {
+            if (vm.responseList.length > vm.responseindex) {
+                vm.response = vm.responseList[vm.responseindex++];
+                console.log(vm.response.options[response.selectedOption - 1].text);
+            }
+        }
+        else
+        {
+            $state
+                .go('root.user');
+        }
     }
 })
 (angular);
